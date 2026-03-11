3446d2f692d93543bec67ac6f912ee95e36ff499
Thahir SK <thahir2005@gmail.com>
2026-03-11 16:25:12 +0000

    Updated application message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("DevOps CI/CD Pipeline Running 🚀");
+  res.send("CI/CD Auto Deployment Working 🚀 Version 2");
 });
 
 app.listen(3000, () => {
